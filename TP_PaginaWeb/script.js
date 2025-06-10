bb2373b562e30c4e5641bdafa93a7370efc99869
´acorneolladhon´ <acorneolladhon@itba.edu.ar>
2025-06-10 18:26:19 -0300

    productos

--- TP_PaginaWeb/script.js
+++ TP_PaginaWeb/script.js
@@ -79,9 +79,451 @@ let productos = [
     imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/05/Imagen-de-WhatsApp-2025-05-24-a-las-10.46.01_47b38177-1024x1024.jpg",
     talles: ["35", "36", "37", "38", "39", "40", "41"]
    },
+  {
+    id: 11,
+    nombre: "Rodilleras Voley Mizuno T10",
+    categoria: "rodilleras",
+    precio: 84999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/rodilleras-mizuno_Mesa-de-trabajo-1.jpg",
+    talles: ["S", "M", "L"]
+   },
+
+  {
+    id: 12,
+    nombre: "Rodilleras de Voley Thorium Confort. Talle único. Simil Nike",
+    categoria: "rodilleras",
+    precio: 39999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/WhatsApp-Image-2023-09-18-at-12.00.25-1.jpeg",
+    talles: ["S", "M", "L"]
+   },
+   
+  {
+    id: 13,
+    nombre: "Rodilleras de Voley Thorium Starter 1.0",
+    categoria: "rodilleras",
+    precio: 16999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/04/Rodi-Thorium-starter.jpg",
+    talles: ["S", "M", "L"]
+   },
+
+  {
+    id: 14,
+    nombre: "Rodilleras Voley Nike Streak Dry Fit",
+    categoria: "rodilleras",
+    precio: 72999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/04/Rodi-nike-streak-0.jpg",
+    talles: ["S", "M", "L"]
+   },
+ 
+  {
+    id: 15,
+    nombre: "Rodilleras Voley Procer WAVE (#289)",
+    categoria: "rodilleras",
+    precio: 18999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/04/Rodi-Procer-289-1.jpg",
+    talles: ["S", "M", "L"]
+   }, 
+   
+  {
+    id: 16,
+    nombre: "Mangas Voley Thorium Lisa",
+    categoria: "manguitas",
+    precio: 12599,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/azul.jpg",
+    talles: ["S", "M", "L"]
+   }, 
+
+  {
+    id: 17,
+    nombre: "Mangas SOX Compresion Graduada",
+    categoria: "manguitas",
+    precio: 15999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/manguitas-sox-colores.png",
+    talles: ["S", "M", "L"]
+   },    
+   
+  {
+    id: 18,
+    nombre: "Calza Oficial 2024 Juego Panteras Selección Argentina LECOQ",
+    categoria: "calzas/shorts",
+    precio: 44999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/Photoroom-20240506_101048.png",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+
+  {
+    id: 19,
+    nombre: "Calza Corta Lisa Thorium",
+    categoria: "calzas/shorts",
+    precio: 21999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/thorium_ml1.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 20,
+    nombre: "Calza Airo Corta Lisa. Tiro Alto",
+    categoria: "calzas/shorts",
+    precio: 19999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/Airo-Calza-azul-marino-scaled-1.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   },    
+
+  {
+    id: 21,
+    nombre: "Short Beach Voley y entrenamiento indoor LeCoq Seleccion Argentina de Voley 2024",
+    categoria: "calzas/shorts",
+    precio: 44999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/1000454607.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+  {
+    id: 22,
+    nombre: "Short Deportivo Thorium",
+    categoria: "calzas/shorts",
+    precio: 19999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/thorium_ml-1.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+  {
+    id: 23,
+    nombre: "Calza corta LECOQ SPORTIF",
+    categoria: "calzas/shorts",
+    precio: 29999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/10/calza-lecoq-1.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+
+  {
+    id: 24,
+    nombre: "Musculosa Beach Voley LeCoq Seleccion Argentina de Voley 2024",
+    categoria: "remeras",
+    precio: 56999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/1000454608.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   },    
+  {
+    id: 25,
+    nombre: "Remera Cuerpo Técnico LeCoq Seleccion Argentina de Voley 2024",
+    categoria: "remeras",
+    precio: 54999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/1000454588.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+
+  {
+    id: 26,
+    nombre: "Remera Entallada Lecoq Sportif Training",
+    categoria: "remeras",
+    precio: 34999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/10/remera-lecoq-dama1.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 27,
+    nombre: "Musculosa Hombre Lecoq Training (Azul Francia)",
+    categoria: "remeras",
+    precio: 32999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/12/muscu-lecoq-training-hombre-francia600.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   },    
+
+  {
+    id: 28,
+    nombre: "Musculosa Hombre Lecoq Sportif Training Negra",
+    categoria: "remeras",
+    precio: 32999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/12/muscu-lecoq-training-hombre-negra600.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   },    
+
+  {
+    id: 29,
+    nombre: "Remera Entallada Sonder Poliester Modelo Tecno",
+    categoria: "remeras",
+    precio: 29999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/Sonder_RemEntallada_tecno_2024_1.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+
+  {
+    id: 30,
+    nombre: "Camiseta Termica Thorium Unisex (Azul",
+    categoria: "remeras",
+    precio: 21999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/PhotoRoom_20230705_132040.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 31,
+    nombre: "Musculosa Entallada Lecoq Sportif Training (VerdeAgua)",
+    categoria: "remeras",
+    precio: 32999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/10/muscu-lecoq-dama1.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 32,
+    nombre: "Remera entrenamiento VOLEY ARGENTINA modelo tango",
+    categoria: "remeras",
+    precio: 21999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/05/Imagen-de-WhatsApp-2025-05-24-a-las-13.38.01_4562c6da.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 33,
+    nombre: "Top Deportivo Airo Cruzado (Colby)",
+    categoria: "tops",
+    precio: 17999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/Top-Airo-Dolby-fucsia-1.jpeg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 34,
+    nombre: "Top Deportivo Thorium",
+    categoria: "tops",
+    precio: 23999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/thorium_ml.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+
+  {
+    id: 35,
+    nombre: "Top LeCoq Panteras Selección Argentina de Voley 2024",
+    categoria: "tops",
+    precio: 44999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/1000454601.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 36,
+    nombre: "Top Deportivo Nike Indy Icon",
+    categoria: "tops",
+    precio: 15799,
+    imagen: "https://www.stockcenter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw6ddedfaa/products/NI_BV4868-661/NI_BV4868-661-1.JPG",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+   
+  {
+    id: 37,
+    nombre: "Top Deportivo Mujer Adidas Don't Rest Alphaskin Gri",
+    categoria: "tops",
+    precio: 24999,
+    imagen: "https://realplaza.vtexassets.com/arquivos/ids/31696904-800-auto?v=638121619327070000&width=800&height=auto&aspect=true",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 38,
+    nombre: "Top deportivo con cierre jaspeado",
+    categoria: "tops",
+    precio: 15999,
+    imagen: "https://d22fxaf9t8d39k.cloudfront.net/077ee12551648165059a63dd7b85b66a34043966e9b626d4b3ec4f36222688cd118911.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 39,
+    nombre: "Top Deportivo Victoria",
+    categoria: "tops",
+    precio: 19999,
+    imagen: "https://http2.mlstatic.com/D_848783-MLA54468935676_032023-O.jpg",
+    talles: ["XS","S", "M", "L", "XL", "2XL"]
+   }, 
+   
+  {
+    id: 40,
+    nombre: "Bolso Gilbert Player Holdall V3",
+    categoria: "bolsos",
+    precio: 92000,
+    imagen: "https://media2.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/b/o/bolso-gilbert-player-holdall-v3-azul-340038302660002-1.jpg",
+    talles: ["único"]
+   },    
+
+  {
+    id: 41,
+    nombre: "Bolso Puma ESS Tote",
+    categoria: "bolsos",
+    precio: 68900,
+    imagen: "https://media2.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/b/o/bolso-puma-ess-tote-mujer-violeta-640030090781002-1.jpg",
+    talles: ["único"]
+   }, 
+
+  {
+    id: 42,
+    nombre: "Bolso Under Armour Undeniable 5.0",
+    categoria: "bolsos",
+    precio: 94999,
+    imagen: "https://media2.solodeportes.com.ar/media/catalog/product/cache/3cb7d75bc2a65211451e92c5381048e9/b/o/bolso-under-armour-undeniable-5-0-negro-78668600-700031369224001-1.jpg",
+    talles: ["único"]
+   }, 
+   
+  {
+    id: 43,
+    nombre: "Bolso Topper Active",
+    categoria: "bolsos",
+    precio: 68999,
+    imagen: "https://media2.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/b/o/bolso-topper-active-gris-800030173208001-1_1.jpg",
+    talles: ["único"]
+   }, 
+   
+  {
+    id: 44,
+    nombre: "Bolso Adidas Linear Duffle",
+    categoria: "bolsos",
+    precio: 48999,
+    imagen: "https://media2.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/b/o/bolso-adidas-linear-duffel-azul-100030in6116001-1.jpg",
+    talles: ["único"]
+   }, 
+ 
+  {
+    id: 45,
+    nombre: "Tobillera Body Care TPA Con Estribo Estabilizador (BC1465)",
+    categoria: "lesiones",
+    precio: 29999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/tobi-bc1465-4.jpg",
+    talles: ["S", "M", "L"]
+   }, 
+  {
+    id: 46,
+    nombre: "Tobillera Body Care TPA (tibio-peroneo-astragalino) (BC1466)",
+    categoria: "lesiones",
+    precio: 29999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/tobi-bc1466-2.jpg",
+    talles: ["S", "M", "L"]
+   }, 
+  {
+    id: 47,
+    nombre: "Tobillera deportiva BODY CARE Runner Pro con tiras (BC1468)",
+    categoria: "lesiones",
+    precio: 29999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/tobi-bc-1468-1.jpg",
+    talles: ["S", "M", "L"]
+   }, 
+   
+  {
+    id: 48,
+    nombre: "Tobillera con cordones y ballenas laterales PROFIT",
+    categoria: "lesiones",
+    precio: 23999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/Picsart_23-09-01_14-42-51-476.jpg",
+    talles: ["S", "M", "L"]
+   }, 
+   
+  {
+    id: 49,
+    nombre: "Muslera Tubular Neoprene Body Care (BC1201)",
+    categoria: "lesiones",
+    precio: 7199,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/muslera.jpg",
+    talles: ["S", "M", "L"]
+   },    
+  {
+    id: 50,
+    nombre: "Rodillera elastic-pro Bander Green",
+    categoria: "lesiones",
+    precio: 5999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/Rodillera-elastic-pro-BanderGreen-1.png",
+    talles: ["S", "M", "L"]
+   },   
+  {
+    id: 51,
+    nombre: "Protectores de dedos Procer (#258)",
+    categoria: "lesiones",
+    precio: 3999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/protector-de-dedos_Mesa-de-trabajo-1.jpg",
+    talles: ["S", "M", "L"]
+   },   
+   
+  {
+    id: 52,
+    nombre: "Bolsa de hielo y agua caliente PROFIT",
+    categoria: "lesiones",
+    precio: 15999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/10/bolsa-agua-profit.jpg",
+    talles: ["Único"]
+   },      
+  {
+    id: 53,
+    nombre: "Venda Deportiva Elástica Profit x3m",
+    categoria: "lesiones",
+    precio: 5599,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/01/Profit-venda2-600.jpg",
+    talles: ["5cm","7cm", "10cm"]
+   }, 
+  {
+    id: 54,
+    nombre: "Cinta / Tela adhesiva SPORT, estribos deportivos",
+    categoria: "lesiones",
+    precio: 6999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/tela-adhesiva-sport.jpg",
+    talles: ["2,5cm","5cm"]
+   },    
+  {
+    id: 55,
+    nombre: "Pelota Pintier Tricolor",
+    categoria: "pelotas",
+    precio: 179999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/04/pelota-piniter-tricolor-1.jpg",
+    talles: ["unico"]
+   },  
+
+  {
+    id: 56,
+    nombre: "Pelota Pintier Bicolor LEAGUE",
+    categoria: "pelotas",
+    precio: 159999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/04/pelota-pintier-league.jpg",
+    talles: ["unico"]
+   },  
+
+  {
+    id: 57,
+    nombre: "Pelota MOLTEN V5M5000",
+    categoria: "pelotas",
+    precio: 139999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/03/molten-5000-1.webp",
+    talles: ["unico"]
+   },  
+   
+  {
+    id: 58,
+    nombre: "Pelota Mikasa V330W",
+    categoria: "pelotas",
+    precio: 149999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2025/04/pelota-mikasa-v330-1-1024x1024.jpg",
+    talles: ["unico"]
+   },  
+  {
+    id: 59,
+    nombre: "Pelota MIKASA Beach Voley Colorfan",
+    categoria: "pelotas",
+    precio: 759999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/11/beachvervio21-4f9220099c3faf1b3416008739708663-240-0.jpg",
+    talles: ["unico"]
+   },   
+
+  {
+    id: 60,
+    nombre: "Kit de retorno REUSCH",
+    categoria: "pelotas",
+    precio: 19999,
+    imagen: "https://voleyotros.com.ar/wp-content/uploads/2024/09/s-l1600-2.jpg",
+    talles: ["unico"]
+   },    
 
 
-    
+
+
+
+
 ];
 
 
